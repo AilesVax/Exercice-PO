12b692acb1a63b2d25e9f47219c92313d36c9257
AilesVax <raphael.vaxelaire10@gmail.com>
2025-11-28 10:16:15 +0100

    premier point

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -5,22 +5,62 @@ class UserModel extends Bdd{
     parent::__construct();
   }
  
-  public function findAll(): array
+ 
+  // public function findOneById(int $id): User | false
+  // {
+  //   $users = $this->co->prepare('SELECT * FROM User WHERE id = :id LIMIT 1');
+  //   $users->setFetchMode(PDO::FETCH_CLASS, 'User');
+  //   $users->execute([
+  //     'id' => $id
+  //   ]);
+ 
+  //   return $users->fetch();
+  // }
+
+  public function logUser(string $email, string $motdepasse): array{
+
+
+
+    $sql = $this->co->prepare('SELECT * from Users WHERE email = :email LIMIT 1');
+    $sql->execute(["email" => $email]);
+    $user = $sql->fetch(PDO::FETCH_ASSOC);
+    if($user && password_verify($motdepasse, $user["motdepasse"])){
+      return [
+            "id"     => $user["id"],
+            "email"  => $user["email"],
+            "prenom" => $user["prenom"],
+            "nom"    => $user["nom"],
+            "role"   => $user["role"]
+        ];
+    }
+
+    
+return [];
+
+}
+
+
+
+public function createUser(array $data) : bool{
+  $sql = $this->co->prepare("INSERT INTO user (prenom,nom,email, motdepasse) VALUES(:prenom,:nom,:email,:motdepasse)");
+  if($sql->execute([
+    'pseudo' => $data['prenom'],
+    'nom' => $data['nom'],
+    'email' => $data['email'],
+    'motdepasse' => $data['motdepasse']
+  ])){
+    return true;
+  }
+return false;
+} 
+
+
+  public function getAllUsers(): array
   {
-    $users = $this->co->prepare('SELECT * FROM User');
+    $users = $this->co->prepare('SELECT prenom FROM User');
     $users->execute();
  
     return $users->fetchAll(PDO::FETCH_CLASS, 'User');
   }
- 
-  public function findOneById(int $id): User | false
-  {
-    $users = $this->co->prepare('SELECT * FROM User WHERE id = :id LIMIT 1');
-    $users->setFetchMode(PDO::FETCH_CLASS, 'User');
-    $users->execute([
-      'id' => $id
-    ]);
- 
-    return $users->fetch();
-  }
+
 }
