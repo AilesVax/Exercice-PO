80820b2601c8a288e9f964a100bf7612836856ed
max <maximilien.ilic@gmail.com>
2025-11-28 11:40:51 +0100

    test

--- app/models/ActiviteModel.php
+++ app/models/ActiviteModel.php
@@ -23,13 +23,16 @@ public function getPlacesLeft(int $activityId): int{
     $places = $this->co->prepare('SELECT places_disponibles FROM activities WHERE id = :id');
     $places->execute(['id' => $activityId]);
     $place = $places->fetchAll(PDO::FETCH_ASSOC);
-    if (!$activityId) {
+
+    if (!$place) {
         return 0;
     }
-    $totalPlaces = (int) $activityId['places_disponibles'];
+    
+    $totalPlaces = (int) $place['places_disponibles'];
     $id = $this->co->prepare('SELECT COUNT(*) as nb_reservations FROM reservations WHERE activite_id = :id AND etat = 1');   
     $id->execute(['id' => $activityId]);
-    $reservation = $id->fetch(PDO::FETCH_ASSOC);    
-    return $totalPlaces - $reservation;
+    $reservation = $id ->fetch(PDO::FETCH_ASSOC);  
+    $place_total = $totalPlaces - $reservation;
+    return max($place_total,0);
     }
 }
